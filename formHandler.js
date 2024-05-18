ce187d912e66ee312846b5b079c4a74f16ef23b3
samiqa_rashid <samiqa.rashid@education.nsw.gov.au>
2024-05-18 19:24:52 +1000

    Making the search bar functional

---
+++ formHandler.js
@@ -0,0 +1,98 @@
+function autocomplete(inp, arr) {
+    var currentFocus;
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(a);
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.addEventListener("click", function(e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            currentFocus++;
+            addActive(x);
+        } else if (e.keyCode == 38) {
+            currentFocus--;
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            e.preventDefault();
+            if (currentFocus > -1) {
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+
+    function addActive(x) {
+        if (!x) return false;
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        x[currentFocus].classList.add("autocomplete-active");
+    }
+
+    function removeActive(x) {
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    document.addEventListener("click", function(e) {
+        closeAllLists(e.target);
+    });
+}
+
+var subjects = ["English", "Math", "Physics", "Biology", "Chemistry", "Economics"];
+var subjectPages = {
+    "English": "english.html",
+    "Math": "maths.html",
+    "Physics": "physics.html",
+    "Biology": "biology.html",
+    "Chemistry": "chemistry.html",
+    "Economics": "economics.html"
+};
+
+autocomplete(document.getElementById("myInput"), subjects);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    // Get the input value
+    var inputValue = document.getElementById("myInput").value.trim();
+    
+    // Check if the input is a valid subject
+    if (inputValue && subjectPages.hasOwnProperty(inputValue)) {
+        // Redirect to the corresponding subject page
+        window.location.href = subjectPages[inputValue];
+    } else {
+        // Redirect to the homepage
+        window.location.href = "Home.html";
+    }
+}
+
